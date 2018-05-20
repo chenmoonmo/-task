2ff46e070fa0b19113298bbed060e57b75ade012
chenmoonmo <36295999+chenmoonmo@users.noreply.github.com>
2018-05-20 19:43:23 +0800

    compare main.js and main2.js

--- task01/js/main.js
+++ task01/js/main.js
@@ -1,35 +1,36 @@
-var timer
+var timer;
 function random(){
     orangecolor();
-    timer=setInterval(getrandomcolor,1000);
+    timer = setInterval(getrandomcolor,1000);
 }
 //生成随机色
 function getrandomcolor(){
-var randomcolor1 = '#' + Math.floor(Math.random()*16777215).toString(16).slice(-6);
-var randomcolor2 = '#' + Math.floor(Math.random()*16777215).toString(16).slice(-6);
-var randomcolor3 = '#' + Math.floor(Math.random()*16777215).toString(16).slice(-6);
+var randomcolor1 = '#' + Math.floor(Math.random()*16777215 + 0.5).toString(16).slice(-6);
+var randomcolor2 = '#' + Math.floor(Math.random()*16777215 + 0.5).toString(16).slice(-6);
+var randomcolor3 = '#' + Math.floor(Math.random()*16777215 + 0.5).toString(16).slice(-6);
 //选择3个随机格子
-randomNum = [];
-for (i=1;i<10;i++) {
-    randomNum.push('box'+i);
+var randomNum = [];
+for (var i=1; i<10; i++) {
+    randomNum.push("box" + i);
 }
 randomNum.sort (
- function(){
-     return 0.5-Math.random()
- })
+ function () {
+     return 0.5 - Math.random()
+ }
+);
  //改变颜色
  document.getElementById(randomNum[0]).style.backgroundcolor=randomcolor1;
  document.getElementById(randomNum[1]).style.backgroundcolor=randomcolor2;
  document.getElementById(randomNum[2]).style.backgroundcolor=randomcolor3;
  for(i=3;i<randomNum.length;i++) {
      document.getElementById(randomNum[i]).style.backgroundcolor="orange";
- };
+ }
 }
 //结束闪
 function orangecolor(){
     clearInterval(timer);
-    var orange=document.getElementsByClassName("box")
-    for(i=1;i<orange.length;i++){
+    var orange=document.getElementsByClassName("box");
+    for(var i=0; i<orange.length; i++){
         orange[i].style.backgroundcolor="orange";
-    };
+    }
 }
