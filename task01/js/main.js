6bd64251719c1ba418d5273f6fc7d5fae7b004ae
chenmoonmo <36295999+chenmoonmo@users.noreply.github.com>
2018-05-21 18:00:10 +0800

    correct error and find new bug
    backgroundcolor-->backgroundColor
    funtion random make a error

--- task01/js/main.js
+++ task01/js/main.js
@@ -1,36 +1,40 @@
 var timer;
-function random(){
-    orangecolor();
-    timer = setInterval(getrandomcolor,1000);
+
+
+function random() {
+    orangeColor();
+    timer = setInterval(getrandomcolor, 1000);
 }
+
 //生成随机色
-function getrandomcolor(){
-var randomcolor1 = '#' + Math.floor(Math.random()*16777215 + 0.5).toString(16).slice(-6);
-var randomcolor2 = '#' + Math.floor(Math.random()*16777215 + 0.5).toString(16).slice(-6);
-var randomcolor3 = '#' + Math.floor(Math.random()*16777215 + 0.5).toString(16).slice(-6);
-//选择3个随机格子
-var randomNum = [];
-for (var i=1; i<10; i++) {
-    randomNum.push("box" + i);
+function getrandomcolor() {
+    var randomcolor1 = '#' + Math.floor(Math.random() * 16777215 + 0.5).toString(16).slice(-6);
+    var randomcolor2 = '#' + Math.floor(Math.random() * 16777215 + 0.5).toString(16).slice(-6);
+    var randomcolor3 = '#' + Math.floor(Math.random() * 16777215 + 0.5).toString(16).slice(-6);
+    //选择3个随机格子
+    var randomNum = [];
+    for (var i = 1; i < 10; i++) {
+        randomNum.push("box" + i);
+    }
+    randomNum.sort(
+        function () {
+            return 0.5 - Math.random()
+        }
+    );
+    //改变颜色
+    document.getElementById(randomNum[0]).style.backgroundColor = randomcolor1;
+    document.getElementById(randomNum[1]).style.backgroundColor = randomcolor2;
+    document.getElementById(randomNum[2]).style.backgroundColor = randomcolor3;
+    for (i = 3; i < randomNum.length; i++) {
+        document.getElementById(randomNum[i]).style.backgroundcolor = "orange";
+    }
 }
-randomNum.sort (
- function () {
-     return 0.5 - Math.random()
- }
-);
- //改变颜色
- document.getElementById(randomNum[0]).style.backgroundcolor=randomcolor1;
- document.getElementById(randomNum[1]).style.backgroundcolor=randomcolor2;
- document.getElementById(randomNum[2]).style.backgroundcolor=randomcolor3;
- for(i=3;i<randomNum.length;i++) {
-     document.getElementById(randomNum[i]).style.backgroundcolor="orange";
- }
-}
+
 //结束闪
-function orangecolor(){
+function orangeColor() {
     clearInterval(timer);
-    var orange=document.getElementsByClassName("box");
-    for(var i=0; i<orange.length; i++){
-        orange[i].style.backgroundcolor="orange";
+    var orange = document.getElementsByClassName("box");
+    for (var i = 0; i < orange.length; i++) {
+        orange[i].style.backgroundColor = "orange";
     }
 }
